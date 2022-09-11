4e9745bd03653e766c7a830cabec252821886624
aliusman3 <ali@pumpjackdataworks.com>
2022-09-11 19:22:57 +0500

    feat: section 7 done

---
+++ src/services/keys.ts
@@ -0,0 +1,4 @@
+export const pageCacheKey = (id: string) => `pagecache#${id}`;
+export const usersKey = (userId: string) => `users#${userId}`;
+export const sessionsKey = (id: string) => `sessions#${id}`;
+export const itemsKey = (id: string) => `items#${id}`;
